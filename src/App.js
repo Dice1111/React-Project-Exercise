b459637dd7fb5e99aec42ed9cacbb30b78e9a402
Dice1111 <kingdice1111@gmail.com>
2022-10-16 11:04:33 +0800

    react_components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 import Header from "./Header";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Navbar />
+      <Footer />
+    </>
+  ) 
 }
 
 export default App;
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './index.css'
+
+function Navbar() {
+  return (
+    <div className='demo'>Navbar</div>
+  )
+}
+
+export default Navbar
